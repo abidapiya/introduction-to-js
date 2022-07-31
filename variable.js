a0b90ef56dc8a6865cd7021d5c7fa1cdda249f99
abidapiya <abidapiya55@gmail.com>
2022-07-31 19:14:38 +0600

    introduction to java

---
+++ variable.js
@@ -0,0 +1,16 @@
+// vary + able = variable
+
+// numeric
+var price = 11;
+var age = 22;
+var temperature = 36;
+
+// string
+var person = "abida piya";
+var location = "himchori cox's bazar";
+var special = 'khairul islam';
+
+// Boolean
+var serious = true;
+var isFullMarks = true;
+var isSingle = false;
